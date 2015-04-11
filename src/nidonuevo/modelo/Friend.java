1d99139d3541dcb1e3b1d857d07c2eab310cb5cf
goldfenrir <jdbustamante@pucp.pe>
2015-04-11 17:07:00 -0500

    holi2

---
+++ src/nidonuevo/modelo/Friend.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nidonuevo.modelo;
+
+/**
+ *
+ * @author TOSHIBA
+ */
+public class Friend {
+    protected int id;
+    protected String name;
+}
