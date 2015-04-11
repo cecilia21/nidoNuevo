08f442be4d7d6306c1f8f0ad1cf052c8230fc0f7
alulab14 <alulab14@V20517.lab.inf.pucp.edu.pe>
2015-04-11 14:10:33 -0500

    3

---
+++ src/nidonuevo/modelo/Item.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nidonuevo.modelo;
+
+/**
+ *
+ * @author alulab14
+ */
+public class Item {
+    protected int id;
+    protected String name;
+    protected int stock;
+    protected String description;
+    
+    
+    
+}
