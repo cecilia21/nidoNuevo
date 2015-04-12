31cedce0b265f51f04534de9221e67fb5231e248
goldfenrir <jdbustamante@pucp.pe>
2015-04-12 18:39:13 -0500

    GF: display

--- src/nidonuevo/model/Engine.java
+++ src/nidonuevo/model/Engine.java
@@ -5,17 +5,27 @@
  */
 package nidonuevo.model;
 import nidonuevo.app.Display;
-public class Engine {
+public class Engine implements Runnable{
     private String title;
     private int width, height;
-    
+    private Display display;
+    private Boolean running=false;
+    private Thread thread;
+    //Input
+    private KeyManager keyManager;
     public Engine(String title, int width, int height){
         this.title = title;
         this.width = width;
-        this.height =height;        
+        this.height =height;  
+        keyManager = new KeyManager();
+        
     }   
     public void start(){
-        Display display=new Display(title,width,height);
+        this.run();
+    }
+    private void init(){
+        display=new Display(title,width,height);
+        display.getFrame().addKeyListener(keyManager); //enlaza el key listener con el frame
     }
     public void getInput(){
         
@@ -28,6 +38,23 @@ public class Engine {
     public void validate(){
         
     }
+    public void run(){
+		
+		init();
+		
+		
+		stop();
+		
+	}
     
-    
+    public synchronized void stop(){
+		if(!running)
+			return;
+		running = false;
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
 }
