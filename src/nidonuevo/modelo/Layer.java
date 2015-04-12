051cbe1cc9a1d710462266eb9ea2ecfff437f5a7
goldfenrir <jdbustamante@pucp.pe>
2015-04-12 15:28:37 -0500

    GF

---
+++ src/nidonuevo/modelo/Layer.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nidonuevo.modelo;
+
+import java.awt.image.BufferedImage;
+
+/**
+ *
+ * @author TOSHIBA
+ */
+public class Layer {
+    private int id;
+    private BufferedImage[] images;
+}
